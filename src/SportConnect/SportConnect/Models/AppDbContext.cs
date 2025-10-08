1e80df5ad0ebfb3c0c97f2151dbfbb22bc01b797
Ayrton Caio <caiodalmune@gmail.com>
2025-10-07 23:22:42 -0300

    commit teste

---
+++ src/SportConnect/SportConnect/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportConnect.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
